5251aeb638baa0d22a4940877ba04e5b9b813156
hassanamjad <hassan@hugenet.ir>
2019-07-10 16:48:46 +0430

    added link to slideshow

--- resources/views/customer/gallery/pictures.blade.php
+++ resources/views/customer/gallery/pictures.blade.php
@@ -3,13 +3,15 @@
     <div class="body-wrapper">
         <div id="content" class="site-content">
             <article>
-                <h2 style="text-align: center">دسته بندی گالری</h2>
+                <h2 style="text-align: center">تصاویر گالری</h2>
                 <br>
                 @foreach($pictures as $picture)
                     <div class="col-md-3">
-                        <img src="{{ asset($picture->picture) }}" style="width: 100%;border-radius: 8px" alt="">
-                        <p>{{ $picture->title1 }}</p>
-                        <p>{{ $picture->title2 }}</p>
+                        <div style="border: 1px solid #AAA">
+                            <img src="{{ asset($picture->picture) }}" style="width: 100%;border-radius: 8px" alt="">
+                            <p>{{ $picture->title1 }}</p>
+                            <p>{{ $picture->title2 }}</p>
+                        </div>
                     </div>
                 @endforeach
             </article>
